75a1ad89b306b52aced92031a1f542a766dc1724
mstyslav <mstyslav2010@gmail.com>
2023-09-22 11:34:15 +0100

    finish project

--- main.js
+++ main.js
@@ -1,13 +1,50 @@
-// const anchors = document.querySelectorAll('a[href*="#"]')
+// Получаем необходимые элементы
+const sliderContainer = document.querySelector('.slider-container');
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const prevBtn = document.querySelector('.prev-slide');
+const nextBtn = document.querySelector('.next-slide');
 
-// for (let anchor of anchors) {
-//     anchor.addEventListener("click", function(event) {
-//         event.preventDefault();
-//         const blockID = anchor.getAttribute('href')
-//         document.querySelector('' + blockID ).scrollIntoView({
-//           behavior: "smooth",
-//           block: "start" 
-//         })
-//     })
+// Инициализируем индекс текущего слайда
+let currentSlide = 0;
+
+// Функция для перемещения слайдов
+function goToSlide(index) {
+  if (index < 0) {
+    currentSlide = slides.length - 1;
+  } else if (index >= slides.length) {
+    currentSlide = 0;
+  }
+  const translateX = -currentSlide * 100;
+  slider.style.transform = `translateX(${translateX}%)`;
+}
+
+// Обработчики событий для кнопок навигации
+prevBtn.addEventListener('click', () => {
+  currentSlide--;
+  goToSlide(currentSlide);
+});
+
+nextBtn.addEventListener('click', () => {
+  currentSlide++;
+  goToSlide(currentSlide);
+});
+
+// Автоматическое переключение слайдов (можете настроить по своему усмотрению)
+// function autoSlide() {
+//   currentSlide++;
+//   goToSlide(currentSlide);
 // }
 
+// // Запуск автоматической прокрутки
+// const autoSlideInterval = setInterval(autoSlide, 1000);
+
+// // Остановка автоматической прокрутки при наведении курсора на слайдер
+// sliderContainer.addEventListener('mouseenter', () => {
+//   clearInterval(autoSlideInterval);
+// });
+
+// // Возобновление автоматической прокрутки при уходе курсора с слайдера
+// sliderContainer.addEventListener('mouseleave', () => {
+//   autoSlideInterval = setInterval(autoSlide, 1000);
+// });
