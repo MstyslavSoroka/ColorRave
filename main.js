5b1a62d920ac65f3e40e3e44fa55a55752246303
mstyslav <mstyslav2010@gmail.com>
2023-09-24 09:17:38 +0100

    fix

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-// Получаем необходимые элементы
 const sliderContainer = document.querySelector('.slider-container');
 const slider = document.querySelector('.slider');
 const slides = document.querySelectorAll('.slide');
 const prevBtn = document.querySelector('.prev-slide');
 const nextBtn = document.querySelector('.next-slide');
 
-// Инициализируем индекс текущего слайда
 let currentSlide = 0;
 
-// Функция для перемещения слайдов
 function goToSlide(index) {
   if (index < 0) {
     currentSlide = slides.length - 1;
@@ -19,7 +16,6 @@ function goToSlide(index) {
   slider.style.transform = `translateX(${translateX}%)`;
 }
 
-// Обработчики событий для кнопок навигации
 prevBtn.addEventListener('click', () => {
   currentSlide--;
   goToSlide(currentSlide);
